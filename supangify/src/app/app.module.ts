1c7e8eaabe49cf35d05b05675c2046bb524b2d66
Pierre <pierre.urbain@5thfloor.be>
2023-10-18 22:33:45 +0200

    integration with supabase

--- supangify/src/app/app.module.ts
+++ supangify/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
